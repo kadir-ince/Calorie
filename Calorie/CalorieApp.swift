7b9e9b7f8baea28fce358798ca9271339c9ff1be
Kadir Ince <kadirincedev@gmail.com>
2020-08-31 16:24:27 +0300

    Initial Commit

---
+++ Calorie/CalorieApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorieApp.swift
+//  Calorie
+//
+//  Created by Kadir on 31.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct CalorieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calorie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calorie
+//
+//  Created by Kadir on 31.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
